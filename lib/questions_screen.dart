77fa134f7cd00b3ad093946fa17f16dac7e65c57
mark churcher <markchurcher@gmail.com>
2023-05-09 07:51:05 +1200

    add question and answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/model/question.model.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +15,22 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("I am the question");
+    final Question currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          ...currentQuestion.answers.map((e) {
+            return AnswerButton(answerText: e, onTap: () {});
+          }).toList(),
+        ],
+      ),
+    );
   }
 }
